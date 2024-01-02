10d9caef53bc8f06089a748a16a2be357e0c615b
sucodetoM <137896231+sucodetoM@users.noreply.github.com>
2024-01-02 15:53:29 -0300

    Update BottomTabNavigator.js

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -36,11 +36,11 @@ export default class BottomTabNavigator extends Component {
               //  iconName = "time";
               //}
 
-              //if (route.name === "Alugue uma Bicicleta") {
-              // iconName = "bicycle";
-              //} else if (route.name === "Histórico de Locações") {
-              //  iconName = "time";
-              //}
+              if (route.name === "Alugue uma Bicicleta") {
+              iconName = "bicycle";
+              } else if (route.name === "Histórico de Locações") {
+             iconName = "time";
+              }
 
               // You can return any component that you like here!
               return (
